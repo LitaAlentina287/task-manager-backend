7a9ebaa5a5ebe70f2e8ad13b44b6d466ecc4cb1a
LitaAlentina287 <alentinalita@gmail.com>
2026-04-25 19:22:07 +0700

    update db.js using env configuration

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'task_manager',
-  password: 'Lita123', // ganti dengan password postgres 
-  port: 5432,
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'task_manager',
+  password: process.env.DB_PASSWORD || 'Lita123',
+  port: process.env.DB_PORT || 5432,
 });
 
 module.exports = pool;
